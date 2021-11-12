61c67cb3950c7559b74b58f205cdd1b40dbdb90d
Sam <RealSamwiseGamgee@protonmail.com>
2021-11-12 23:19:25 +0800

    implement faucet initialize

--- programs/faucet/src/lib.rs
+++ programs/faucet/src/lib.rs
@@ -1,14 +1,75 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("JDnDwBLCw8nnvbrhqT2eyngWGqAapRxBuJcCmMYTbbE2");
 
 #[program]
 pub mod faucet {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(ctx: Context<Initialize>, nonce: u8, drip_volume: u64) -> ProgramResult {
+        let config = &mut ctx.accounts.config;
+        config.token_program = *ctx.accounts.token_program.key;
+        config.token_mint = *ctx.accounts.token_mint.key;
+        config.token_authority = *ctx.accounts.token_authority.key;
+        config.nonce = nonce;
+        config.drip_volume = drip_volume;
+        Ok(())
+    }
+
+    pub fn drip(ctx: Context<Drip>) -> ProgramResult {
+        let config = ctx.accounts.config.clone();
+
+        let cpi_program = ctx.accounts.token_program.clone();
+        let cpi_accounts = token::MintTo {
+            mint: ctx.accounts.token_mint.to_account_info(),
+            to: ctx.accounts.receiver.to_account_info(),
+            authority: ctx.accounts.token_authority.to_account_info(),
+        };
+        let seeds = &[config.to_account_info().key.as_ref(), &[config.nonce]];
+        let signer_seeds = &[&seeds[..]];
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+        token::mint_to(cpi_ctx, config.drip_volume)?;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 32 + 32 + 32 + 1 + 8)]
+    pub config: Account<'info, Config>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account("token_program.key == &token::ID")]
+    pub token_program: AccountInfo<'info>,
+
+    pub token_mint: AccountInfo<'info>,
+
+    pub token_authority: AccountInfo<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Drip<'info> {
+    #[account()]
+    pub config: Account<'info, Config>,
+
+    pub token_program: AccountInfo<'info>,
+    pub token_mint: AccountInfo<'info>,
+    pub token_authority: AccountInfo<'info>,
+
+    #[account(mut)]
+    pub receiver: AccountInfo<'info>,
+}
+
+#[account]
+pub struct Config {
+    token_program: Pubkey,
+    token_mint: Pubkey,
+    token_authority: Pubkey,
+    nonce: u8,
+    drip_volume: u64,
+}
--- tests/faucet.ts
+++ tests/faucet.ts
@@ -1,17 +1,56 @@
-import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
-import { Faucet } from '../target/types/faucet';
+import * as anchor from "@project-serum/anchor";
+import assert from "assert";
+import { Faucet } from "../target/types/faucet";
+import { Program } from "@project-serum/anchor";
+import { TokenInstructions } from "@project-serum/serum";
+import { createMint } from "@project-serum/common";
 
-describe('faucet', () => {
+describe("faucet", () => {
+  const provider = anchor.Provider.local();
 
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Faucet as Program<Faucet>;
+  const tokenDecimals = 9;
+  const dripVolume = new anchor.BN(10 ** tokenDecimals);
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  let config: anchor.web3.Keypair;
+  let tokenAuthority: anchor.web3.Keypair;
+  let tokenMint: anchor.web3.Keypair;
+  let nonce: number;
+
+  before(async () => {
+    config = anchor.web3.Keypair.generate();
+    [tokenAuthority, nonce] = await anchor.web3.PublicKey.findProgramAddress(
+      [config.publicKey.toBuffer()],
+      program.programId
+    );
+    tokenMint = await createMint(provider, tokenAuthority, tokenDecimals);
+  });
+
+  describe("initialize", () => {
+    it("success", async () => {
+      console.log(TokenInstructions.TOKEN_PROGRAM_ID.toBase58());
+      await program.rpc.initialize(nonce, dripVolume, {
+        accounts: {
+          config: config.publicKey,
+          user: provider.wallet.publicKey,
+          tokenProgram: TokenInstructions.TOKEN_PROGRAM_ID,
+          tokenMint: tokenMint,
+          tokenAuthority: tokenAuthority,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [config],
+      });
+
+      const configAccount = await program.account.config.fetch(
+        config.publicKey
+      );
+      assert.strictEqual(
+        configAccount.tokenProgram.toBase58(),
+        TokenInstructions.TOKEN_PROGRAM_ID.toBase58()
+      );
+    });
   });
 });
